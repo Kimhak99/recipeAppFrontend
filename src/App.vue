9238cc581164c63f537b8c28bb14679bb48dc35b
Kimhak99 <kolkimhak99@yahoo.com>
2021-09-16 17:01:20 +0700

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
       color="primary"
       dark
     > -->
+    <v-app>
        <v-card flat>
     <v-container fluid>
       <v-row class="child-flex">
         <div>
-          <v-toolbar>
+          <v-toolbar color="#B71C1C" dark>
             <!-- <v-btn
               icon
               class="hidden-xs-only"
@@ -17,7 +18,7 @@
               <v-icon>mdi-arrow-left</v-icon>
             </v-btn> -->
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
-            <v-toolbar-title>The Avid Foodie's</v-toolbar-title>
+            <v-toolbar-title>The Foodie's Tongue</v-toolbar-title>
 
             <v-spacer></v-spacer>
           </v-toolbar>
@@ -27,18 +28,31 @@
           <v-toolbar dark>
             <v-spacer></v-spacer>
 
-            <v-btn icon>
-              <v-icon>mdi-reply</v-icon>
+            <v-btn flat>
+              Sign Up
             </v-btn>
 
             <v-btn icon>
-              <v-icon>mdi-dots-vertical</v-icon>
+              Log In
             </v-btn>
           </v-toolbar>
         </div>
       </v-row>
+      <v-row>
+         <v-content>
+            <hello-world/>
+          </v-content>
+      </v-row>
+    
+        <v-footer class="pa-3" color="#B71C1C" dark>
+          <v-spacer></v-spacer>
+          <div>The Foodie's Tongue &nbsp; &copy; {{new Date().getFullYear()}}</div>
+          <v-spacer></v-spacer>
+        </v-footer>
+     
     </v-container>
   </v-card>
+  </v-app>
     <!-- </v-app-bar>
 
     <v-main>
@@ -48,8 +62,10 @@
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
+  components: { HelloWorld },
   name: 'App',
 
   data: () => ({
