158c28004e6158ad66211ce85291d6fe99ca1723
Kimhak <kolkimhak99@yahoo.com>
2023-04-07 08:47:56 -0700

    update basic config file

--- src/utils/basicConfig.js
+++ src/utils/basicConfig.js
@@ -1,7 +1,10 @@
 
 const basicConfig = {
-    api_url: "http://localhost:5000",
-    file_url: "http://localhost:5000/file/",
+    // api_url: "http://localhost:3000",
+    // file_url: "http://localhost:3000/file/",
+    api_url: "https://recipe-app-three-chi.vercel.app",
+    file_url: "https://recipe-app-three-chi.vercel.app/file/",
+    
     blank_profile_img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL49P6tmh4jYu3SItDJZaFocVQKPYOB22_Dg&usqp=CAU"
 }
 
